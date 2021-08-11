63e7d4682203b14aa29ad7393ee1defc6ddd199c
phucdo2601 <ngocphucdo2601@gmail.com>
2021-08-11 22:31:09 +0700

    Demo Paypal in Spring boot

---
+++ spring-paypal-demo/src/main/java/com/phucdn/paypal/SpringPaypalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.phucdn.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalDemoApplication.class, args);
+	}
+
+}
---
+++ spring-paypal-demo/src/test/java/com/phucdn/paypal/SpringPaypalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdn.paypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
